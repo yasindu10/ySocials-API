9496fd66603803037f877387f31c3b55fa364aca
yasindu10 <yasindusamarasinghe10@gmail.com>
2024-03-18 21:43:23 +0530

    add test route

--- server/app.js
+++ server/app.js
@@ -21,6 +21,10 @@ app.use(cors({ origin: "*" }))
 
 firebase.initializeApp(require('./firebase/config'))
 
+app.use((req, res) => {
+    res.send('hii')
+})
+
 app.use('/api/v1/auth', authRouter)
 app.use(authorization) // authorization
 app.use('/api/v1/post', postRouter)
