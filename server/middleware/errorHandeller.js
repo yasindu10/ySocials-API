84508617f80c1555216fe85a9c137aef5882059d
yasindu10 <yasindusamarasinghe10@gmail.com>
2024-03-18 22:31:09 +0530

    add error logger

--- server/middleware/errorHandeller.js
+++ server/middleware/errorHandeller.js
@@ -4,6 +4,7 @@ const errorHandeller = (err, req, res, next) => {
     if (err instanceof CustomError)
         return res.status(err.stateCode).json({ success: false, msg: err.message })
 
+    console.log(err.message)
     res.status(500).json({ success: false, msg: err.message })
 }
 
